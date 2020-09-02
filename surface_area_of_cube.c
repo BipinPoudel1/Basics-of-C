d6b8de1b5d1bb61cbbab6b4efc5e092b50a97a83
BipinPoudel1 <63181387+BipinPoudel1@users.noreply.github.com>
2020-09-02 15:00:53 +0545

    Add files via upload

---
+++ surface_area_of_cube.c
@@ -0,0 +1,11 @@
+///To calculate the surface area of cube.
+#include <stdio.h>
+int main()
+{
+   int l,A;
+   printf("Enter the length~ \n");
+   scanf("%d",&l);
+   A=6*l*l;
+   printf("Surface area= %d",A);
+   return 0;
+}
